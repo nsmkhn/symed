babb18896ed8555d8b5cc63f01f794d175e7ff88
Nikita Tokariev <mykyta.tokariev@gmail.com>
2021-10-02 20:24:57 +0300

    Implemented encryption in hex

--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,4 @@
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 def tabula_recta(s):
     tabula_recta = []
@@ -16,7 +16,32 @@ def encrypt_tr(plaintext, keyword, tr):
     for i, letter in enumerate(plaintext):
         ciphertext += tr[key[i]][letter]
     return ciphertext
-    
+
+def encrypt_alg(plaintext, keyword, alphabet):
+    key = keyword * (len(plaintext) // len(keyword) + 1)
+    ciphertext = ''
+    for i, letter in enumerate(plaintext):
+        ciphertext += alphabet[((alphabet.find(letter) + alphabet.find(key[i])) % len(alphabet))]
+    return ciphertext
+
+def encrypt_hex(plaintext, keyword):
+    key = keyword * (len(plaintext) // len(keyword) + 1)
+    plaintext_hex = ''.join(hex(ord(c))[2:] for c in plaintext)
+    key_hex = ''.join(hex(ord(c))[2:] for c in key)
+    ciphertext = bytearray()
+    for i in range(0, len(plaintext_hex), 2):
+        ciphertext += bytearray.fromhex(hex(int(plaintext_hex[i:i+2], 16) ^ int(key_hex[i:i+2], 16))[2:].zfill(2))
+    return ciphertext.decode(encoding='ascii')
+
+def decrypt_hex(ciphertext, keyword):
+    key = keyword * (len(ciphertext) // len(keyword) + 1)
+    ciphertext_hex = ''.join(hex(ord(c))[2:].zfill(2) for c in ciphertext)
+    key_hex = ''.join(hex(ord(c))[2:] for c in key)
+    plaintext = bytearray()
+    for i in range(0, len(ciphertext_hex), 2):
+        plaintext += bytearray.fromhex(hex(int(ciphertext_hex[i:i+2], 16) ^ int(key_hex[i:i+2], 16))[2:].zfill(2))
+    return plaintext.decode(encoding='ascii')
+
 def decrypt_tr(ciphertext, keyword, tr):
     key = keyword * (len(ciphertext) // len(keyword) + 1)
     plaintext =  ''
@@ -24,26 +49,24 @@ def decrypt_tr(ciphertext, keyword, tr):
         plaintext += list(tr[key[i]].keys())[list(tr[key[i]].values()).index(letter)]
     return plaintext
 
-def encrypt_alg(plaintext, keyword, alphabet):
-    key = keyword * (len(plaintext) // len(keyword) + 1)
-    ciphertext = ''
-    for i, letter in enumerate(plaintext):
-        ciphertext += alphabet[((ord(letter) + ord(key[i])) % len(alphabet))]
-    return ciphertext
-
 def decrypt_alg(ciphertext, keyword, alphabet):
     key = keyword * (len(ciphertext) // len(keyword) + 1)
     plaintext = ''
     for i, letter in enumerate(ciphertext):
-        plaintext += alphabet[(ord(letter) - ord(key[i]) + len(alphabet)) % len(alphabet)]
+        plaintext += alphabet[(alphabet.find(letter) - alphabet.find(key[i]) + len(alphabet)) % len(alphabet)]
     return plaintext
 
 def hack(ciphertext):
     pass
 
 ct = encrypt_tr('ATTACKATDAWN', 'LEMON', TABULA_RECTA)
-print("Ciphertext: ", ct)
-ct = encrypt_alg('ATTACKATDAWN', 'LEMON', ALPHABET)
-print("Ciphertext: ", ct)
-print("Plaintext: ", decrypt_tr(ct, 'LEMON', TABULA_RECTA))
-print("Plaintext: ", decrypt_alg(ct, 'LEMON', ALPHABET))
+print('Ciphertext:', ct)
+print('Plaintext:', decrypt_tr(ct, 'LEMON', TABULA_RECTA))
+
+ct = encrypt_alg('attackatdawn', 'LEMON', ALPHABET)
+print('Ciphertext:', ct)
+print('Plaintext:', decrypt_alg(ct, 'LEMON', ALPHABET))
+
+ct = encrypt_hex('attackatdawn', 'LEMON')
+print('Ciphertext:', ct)
+print('Plaintext:', decrypt_hex(ct, 'LEMON'))
